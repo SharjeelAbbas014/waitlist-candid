4944e02d2cbc49a85625b6ead07eed73d48d55fb
Sharjeel <sharjeel@upcastr.co>
2024-03-03 14:26:51 +0500

    init

--- components/Atoms/Title.tsx
+++ components/Atoms/Title.tsx
@@ -7,8 +7,9 @@ interface TitleProps {
 const Title: React.FC<TitleProps> = ({ label }) => {
   return (
     <div className="relative center w-fit mx-auto mb-16">
-      <span className="absolute mw-[150%] h-[5px] bg-primary rounded top-3 -left-1 -right-1 z-10" />
-      <h2 className="text-[1.1rem] xl:text-2xl font-semibold uppercase relative z-20">{label}</h2>
+      <h2 className="text-[1.1rem] xl:text-2xl font-semibold uppercase relative z-20 bg-clip-text text-transparent bg-gradient-to-r from-purple-500 to-teal-600">
+        {label}
+      </h2>
     </div>
   );
 };
